eeaab3da7f87d615010ee166be4adb16594db07e
apple <apple@apples-MacBook-Pro.local>
2025-06-17 01:34:25 +0530

    new project

---
+++ cypress/e2e/register-flow.cy.js
@@ -0,0 +1,20 @@
+describe('Register Flow', () => {
+
+    it('Enter from the detail', () => {
+        // Arrange
+        cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/register');
+        cy.get('#input-firstname').type('Uday');
+        cy.get('#input-lastname').type('QA');
+        cy.get('#input-telephone').type('8871938073');
+        cy.get('#input-email').type('mishrauday138@gmail.com');
+        cy.get('#input-password').type('password123');
+        cy.get('#input-confirm').type('password123');
+        cy.get('input[type = radio]').eq(1).check();
+        cy.get("input[type ='checkbox']").check();
+        cy.get('input.btn-primary').click();
+    
+    
+    
+    })
+    
+});
